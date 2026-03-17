7a39036c5af5bdecafcd97aa76018a2d144c8c0e
Daryl <darylfredricia@gmail.com>
2026-03-17 18:32:34 -0500

    Checkpoint 1 part 2

--- p102/vite.config.js
+++ p102/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/p102/',
+  base: process.env.NODE_ENV === 'production' ? '/p102/' : '/',
   plugins: [react()],
   build: {
     outDir: "../docs",
